32a7dac9acf58ced009defd6671bc0caa0dde438
Tomas Bregoli <tbregoli@mail.austral.edu.ar>
2026-08-19 17:09:20 -0300

    changed tailwind to v4.3.3

--- src/pages/ExampleHomepage.tsx
+++ src/pages/ExampleHomepage.tsx
@@ -1,6 +1,6 @@
-export default function ExampleHomepage(){
+export default function ExampleHomepage() {
     return (
-        <h1 className="text-6xl md:text-8xl font-serif font-bold tracking-tight leading-none text-center">
+        <h1 className="text-6xl md:text-8xl font-serif text-blue-400 font-bold tracking-tight leading-none text-center">
             This is the homepage.
         </h1>
     )
