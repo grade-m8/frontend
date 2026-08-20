bdd4a37ce8cd29864708d3447c52fb03c9b502fb
Tomas Bregoli <tbregoli@mail.austral.edu.ar>
2026-08-20 20:55:54 -0300

    added linter, formatter and git hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ExampleHomepage from "./pages/ExampleHomepage.tsx";
 import AlternativePage from "./pages/AlternativePage.tsx";
 
 function App() {
   return (
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<ExampleHomepage/>}/>
-          <Route path="/alternative" element={<AlternativePage/>}/>
-        </Routes>
-      </BrowserRouter>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<ExampleHomepage />} />
+        <Route path="/alternative" element={<AlternativePage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
