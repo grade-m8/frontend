10abb946b5bbbaf26401f1f61090972193dd9a7d
renki <renkineluciano1@gmail.com>
2026-09-13 17:43:03 -0300

    changed role type to Role

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,12 +1,13 @@
 import { httpsCallable } from "firebase/functions";
 import { functions } from "./firebase";
+import type { Role } from "@/types/role";
 
 export interface UserProfile {
   uid: string;
   email: string;
   firstName: string;
   lastName: string;
-  role: string;
+  role: Role;
 }
 
 export async function getProfile(): Promise<UserProfile> {
