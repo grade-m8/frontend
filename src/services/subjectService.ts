2b0bfeb450444fc4ef194f90d00572a706c5bc27
tomibrg <tbregoli@mail.austral.edu.ar>
2026-09-14 13:41:24 -0300

    changed subjectService to use funcitions and added functions to the client

--- src/services/subjectService.ts
+++ src/services/subjectService.ts
@@ -1,22 +1,14 @@
 import type { Subject } from "@/types/subject.ts";
 import type { User } from "firebase/auth";
-
-const baseUrl: string = import.meta.env.VITE_BASE_URL;
+import { httpsCallable } from "firebase/functions";
+import { functions } from "./firebase";
 
 export async function listOwned(user: User): Promise<Subject[]> {
   if (!user) throw new Error("failed to fetch subjects");
-  const token = await user.getIdToken();
-  const payload = { data: {} };
-
-  const response = await fetch(baseUrl + "/listOwnedSubjects", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${token}`, // Inform the server about the data format
-    },
-    body: JSON.stringify(payload),
-  });
-  if (!response.ok) throw new Error("failed to fetch subjects");
-  const responseData = await response.json();
-  return responseData.result;
+  const getOwnedSubjects = httpsCallable<void, Subject[]>(
+    functions,
+    "listOwnedSubjects",
+  );
+  const response = await getOwnedSubjects();
+  return response.data;
 }
