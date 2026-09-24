24116eecc00287817e20d5436a2e1dd1697d7ca0
tomibrg <tbregoli@mail.austral.edu.ar>
2026-09-24 18:30:41 -0300

    mock data to see the testing route for the component

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -6,8 +6,6 @@ import RedirectIfAuthenticated from "@/routes/RedirectIfAuthenticated.tsx";
 import SubjectExamsPage from "@/pages/Subjects/SubjectExamsPage";
 import ErrorPage from "@/pages/ErrorPage.tsx";
 import { ExamConfigPage } from "@/pages/exams/ExamEditor";
-import QuestionCard from "@/components/forms/QuestionCard.tsx";
-import type { Question } from "@/types/exam.ts";
 
 export default function AppRouter() {
   return (
@@ -22,38 +20,8 @@ export default function AppRouter() {
             path="/teacher/exams/:examId/config"
             element={<ExamConfigPage />}
           />
-          <Route
-            path="testingCardEdit"
-            element={
-              <QuestionCard
-                question={fakeQuestionCardProps.question}
-                index={fakeQuestionCardProps.index}
-                isEditing={fakeQuestionCardProps.isEditing}
-                onEdit={fakeQuestionCardProps.onEdit}
-                onSave={fakeQuestionCardProps.onSave}
-                onCancel={fakeQuestionCardProps.onCancel}
-                onDelete={fakeQuestionCardProps.onDelete}
-              />
-            }
-          />
-          <Route
-            path="testingCardRead"
-            element={
-              <QuestionCard
-                question={fakeQuestionCardProps.question}
-                index={fakeQuestionCardProps.index}
-                isEditing={false}
-                onEdit={fakeQuestionCardProps.onEdit}
-                onSave={fakeQuestionCardProps.onSave}
-                onCancel={fakeQuestionCardProps.onCancel}
-                onDelete={fakeQuestionCardProps.onDelete}
-              />
-            }
-          />
+          <Route path="/403" element={<ErrorPage />} />
         </Route>
-
-        <Route path="/403" element={<ErrorPage />} />
-
         <Route
           element={
             <ProtectedRoutes allowedRoles={["Student", "Admin", "Professor"]} />
@@ -70,25 +38,3 @@ export default function AppRouter() {
     </BrowserRouter>
   );
 }
-
-const fakeQuestion: Question = {
-  questionId: "1",
-  order: 2,
-  title: "Desarrollo Temático",
-  prompt: "This is a prompt",
-  points: 4,
-  idealAnswer: "This is an ideal answer",
-};
-
-const fakeQuestionCardProps = {
-  question: fakeQuestion,
-  index: 1,
-  isEditing: true,
-  onEdit: (questionId: string) =>
-    console.log(`Clicked edit button! id: ${questionId}`),
-  onDelete: (questionId: string) =>
-    console.log(`Clicked delete button! id: ${questionId}`),
-  onSave: (updateQuestion: Question) =>
-    console.log(`Clicked save button! question: ${updateQuestion}`),
-  onCancel: () => console.log("Clicked save button!"),
-};
